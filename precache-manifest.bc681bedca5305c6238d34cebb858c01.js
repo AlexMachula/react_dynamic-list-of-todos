d2ec51647be70d630c4159db345eaaae138298fb
Alex Machula <alexmachulamail@gmail.com>
2020-05-19 12:38:33 +0300

    Updates

---
+++ precache-manifest.bc681bedca5305c6238d34cebb858c01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614bd1997f8d31e11d9ba9f1960c25bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbf02cfbe08d0670deb3",
+    "url": "./static/js/2.e505060b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e505060b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e56b3bf2e85bab1d2e",
+    "url": "./static/js/main.f25a3657.chunk.js"
+  },
+  {
+    "revision": "319a3c468d8ecd2a5698",
+    "url": "./static/js/runtime-main.e5e6300a.js"
+  }
+]);
